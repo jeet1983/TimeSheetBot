7ad534ea7dc85cfcfbf5d90bc5d045e0d467ec87
jeet1983 <sengupta.prosenjit@gmail.com>
2017-08-28 09:11:43 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,22 +1,13 @@
-function getuserInput(txt) {
-  var rettxt=1;
+function getuserCInput(txt) {
+  var rettxt='';
  if(txt.indexOf('Create')!=-1)
-    rettxt=1;
- if(txt.indexOf('Query')!=-1)
-    rettxt=2;
+    rettxt='C';
   return rettxt;
 }
 
-function isCreate(txt) {
-var retval=0;
- if(txt.indexOf('Create')!=-1)
-    retval=1;
-  return retval;
-}
-function isQuery(txt) {
-var retval=0;
+function getuserQInput(txt) {
+  var rettxt='';
  if(txt.indexOf('Query')!=-1)
-    retval=1;
-  return retval;
+    rettxt='Q';
+  return rettxt;
 }
-
