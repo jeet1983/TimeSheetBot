65d152422961a706e9022405be689c5190959c55
jeet1983 <sengupta.prosenjit@gmail.com>
2017-08-28 08:54:27 +0530

    Update common.js

--- common.js
+++ common.js
@@ -6,3 +6,17 @@ function getuserInput(txt) {
     rettxt='Query';
   return rettxt;
 }
+
+function isCreate(txt) {
+var retval=0;
+ if(txt.indexOf('Create')!=-1)
+    retval=1;
+  return retval;
+}
+function isQuery(txt) {
+var retval=0;
+ if(txt.indexOf('Query')!=-1)
+    retval=1;
+  return retval;
+}
+
